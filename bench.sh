121461616b4cb124895eb3ce231ea7eec5ae8a7a
Leonid Beschastny <lbeschastny@iknow.travel>
2015-04-17 12:28:58 +0300

    Refactored bench.sh

--- bench.sh
+++ bench.sh
@@ -1,41 +1,27 @@
 #!/bin/bash
 
+function install {
+  npm install $@ --silent &> /dev/null
+}
+
 function init {
   rm -rf ~/.npm
   rm -rf ./node_modules
-  npm install --silent &> /dev/null
-}
-
-function node_10 {
-  n 0.10.30
-}
-
-function node_12 {
-  n 0.12.0
-}
-
-function mongoose_3 {
-  npm i mongoose@3.8 --silent &> /dev/null
-}
-
-function mongoose_4 {
-  npm i mongoose@4 --silent &> /dev/null
+  n $1
+  install
 }
 
 function run {
+  install mongoose@$1
   npm start --silent
   echo ""
 }
 
-node_10
-init
-mongoose_3
-run
-mongoose_4
-run
-node_12
-init
-mongoose_3
-run
-mongoose_4
-run
+function run_all {
+  init $@
+  run 3.8
+  run 4.0
+}
+
+run_all 0.10.30
+run_all 0.12.0
